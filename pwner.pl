960ad0b354f0d417a0e522c80f90772e04afa2f8
Robert Jacobsen <robertj@opera.com>
2012-08-25 14:26:47 +0200

    Daemonise the bot.

--- pwner.pl
+++ pwner.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 
+use POSIX;
 use Bot::BasicBot::Pluggable;
 use Getopt::Long;
 
@@ -19,12 +20,16 @@ $nick       //= "Pwner";    # The bot's nick
 
 die 'Usage ./pwner.pl --nick <nickname> --server <servername> --port <port number> --channels "<#channel1,#channel2>"' unless $server && $port && $channels && $nick;
 
+my $pid = fork();
+exit if $pid;
+POSIX::setsid();
+
 my $bot = Bot::BasicBot::Pluggable->new(
     server      => $server,
     port        => $port,
     channels    => [ split ',', $channels ],
     nick        => $nick,
-    username    => 'pwner',
+    username    => lc $nick,
     charset     => "utf-8",
 );
 
